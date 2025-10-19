cf3565668124fa6cf1214465b520d2cbd0784451
root <root@DESKTOP-0V4NUDQ.localdomain>
2025-10-19 08:22:44 -0300

    fix:teste final deploy11

--- Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Configuracao/SecurityConfig.java
+++ Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Configuracao/SecurityConfig.java
@@ -1,4 +1,4 @@
-﻿package br.com.nilson.AppCadastroPessoas.Configuracao;
+package br.com.nilson.AppCadastroPessoas.Configuracao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
