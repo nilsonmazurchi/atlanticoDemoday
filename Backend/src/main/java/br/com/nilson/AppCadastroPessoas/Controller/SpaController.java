9cacbb6cd978cc84fa169a2aa5a0238795609482
root <root@DESKTOP-0V4NUDQ.localdomain>
2025-10-20 20:50:26 -0300

    fix:teste deploy56

--- Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Controller/SpaController.java
+++ Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Controller/SpaController.java
@@ -1,11 +1,24 @@
+package br.com.nilson.AppCadastroPessoas.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
     
-    @RequestMapping(value = {"/", "/home", "/cadastro", "/pessoas"})
+    /**
+     * Redireciona todas as rotas do Angular para o index.html
+     * O Angular vai cuidar do roteamento no client-side
+     */
+    @RequestMapping(value = {
+        "/",
+        "/home",
+        "/cadastro", 
+        "/pessoas",
+        "/{path:^(?!api|static|h2-console|.*\\..*).*$}/**"
+    })
     public String redirect() {
-        return "forward:/";
+        // Forward para index.html - NÃO usar forward:/ que cria loop
+        return "forward:/index.html";
     }
 }
