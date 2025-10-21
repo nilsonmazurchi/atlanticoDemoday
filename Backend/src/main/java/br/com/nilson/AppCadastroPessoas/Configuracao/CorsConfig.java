ff4a63d4fa0c0baf4981ceebccc619c50b75150b
root <root@DESKTOP-0V4NUDQ.localdomain>
2025-10-20 21:16:39 -0300

    fix:teste deploy57

--- Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Configuracao/CorsConfig.java
+++ Backend/src/main/java/br/com/nilson/AppCadastroPessoas/Configuracao/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:4200", "http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")                                    )
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
